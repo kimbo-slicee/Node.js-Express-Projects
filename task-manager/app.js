2c8ed29f62e243f7b51a4584e01ce4c736e0d5dc
mohammed el aouri <mohammedelaouri@gmail.com>
2024-11-07 01:44:25 +0100

    in this commit i connect my app to MongoDb atlas database and i started to create the schema of tasks

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,12 +1,22 @@
+require('dotenv').config();
 const express=require('express');
 const app=express();
 const port =5000;
 const tasks=require('./Router/routers');
 const { swaggerUi, swaggerDocs } = require("./env/swagger");
+const connectDB= require('./DB/mongoDB');
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api/v1/tasks',tasks);
-app.listen(port,()=>{
-    console.log(`Server Listening in Port ${port}`);
-    console.log("Swagger docs available at http://localhost:3000/api-docs");
+const start=async ()=>{
+    try {
+       await connectDB(process.env.MONGO_URI);
+        app.listen(port,()=>{
+            console.log(`Server Listening in Port ${port}`);
+            console.log("Swagger docs available at http://localhost:3000/api-docs");
+        });
 
-});
+    }catch (error){
+        console.log(error)
+    }
+}
+start();
