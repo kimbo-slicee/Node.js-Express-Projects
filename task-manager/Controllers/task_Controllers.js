0bdc78d43b1b856300d2e3f79a80feb73c6e9c87
mohammed el aouri <mohammedelaouri@gmail.com>
2024-11-09 15:04:31 +0100

    Refactoring The Controllers

--- task-manager/Controllers/task_Controllers.js
+++ task-manager/Controllers/task_Controllers.js
@@ -10,35 +10,24 @@ const getOneTask= asyncWrapper( async (req,res)=>{
     if(!task) return res.status(400).json({ErrorMessage:"TASK NOTE FOUND"});
     res.status(200).json({task});
 });
-const createTask=async (req,res)=>{
-    try{
+const createTask=asyncWrapper( async (req,res)=>{
     const task =await Task.create(req.body);
     res.status(200).json(task);
-    }catch (error){
-        res.status(500).json({error})
-    }
-}
-const upDateTask= async (req,res)=>{
-    try{
+
+})
+const upDateTask=asyncWrapper( async (req,res)=>{
    const {id:taskID}=req.params;
    const task= await Task.findByIdAndUpdate({_id:taskID},req.body);
    if(!task) return res.status(400).json({error:"TASK NOTE FOUND"});
    res.status(200).json({task:task});
 
-    }catch(error){
-        res.status(500).json({success:false,msg:"DATA BASE ERROR"});
-    }
-}
-const deleteTask=async (req,res)=>{
-try{
+});
+const deleteTask=asyncWrapper( async (req,res)=>{
     const {id:taskID}=req.params;
     const task=await Task.findOneAndDelete({_id:taskID})
     if(!task) return res.status(400).json({ErrorMessage:"TASK NOTE FOUND"});
     res.status(200).json({task:null, status:'success'});
-}catch (error){
-    res.status(500).json({error})
-}
-}
+})
 
 module.exports={
     getAllTasks,
@@ -46,4 +35,4 @@ module.exports={
     createTask,
     upDateTask,
     deleteTask
-}
+}  
